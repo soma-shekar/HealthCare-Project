bb4643037e534d08f90ccbdfd3d12986af19d427
Shekar <gsomashekaryadav@gmail.com>
2022-12-21 13:11:54 +0530

    commited SpecializationRepo Repository

---
+++ src/main/java/com/sbi/soma/repo/SpecializationRepo.java
@@ -0,0 +1,9 @@
+package com.sbi.soma.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sbi.soma.entity.Specialization;
+
+public interface SpecializationRepo  extends JpaRepository<Specialization, Long>{
+
+}
